c05353e99877b8498dfd24f65c56ef3148b7a809
Andy <andy@gaoding.com>
2018-05-22 11:34:16 +0800

    添加Fragment中隐藏代码

--- hideSoftInputFromWindow.java
+++ hideSoftInputFromWindow.java
@@ -1,6 +1,9 @@
+/*
+ * 在Activity中
+ */
 public class BaseActivity extends AppCompatActivity {
 	// ...
-	
+
     /**
      * 点击空白位置 隐藏软键盘
      */
@@ -12,4 +15,24 @@ public class BaseActivity extends AppCompatActivity {
         }
         return super.onTouchEvent(event);
     }
+}
+
+/*
+ * 在 fragment中
+ */
+public View onCreateView(Bundle saveInstance, View parent){
+			View rootView = LayoutInflater.from(mContext).inflate(R.layout.root_view, parent, false);
+			rootView.setOnTouchListener(new OnTouchListener(){
+					@Override
+					public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager manager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
+                    if(getActivity().getCurrentFocus()!=null && getActivity().getCurrentFocus().getWindowToken()!=null){
+                      manager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                    }
+                 }
+               return false;
+           }
+				 });
+			return rootView; 
 }
